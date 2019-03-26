ba72d8ad863d149543250479502982a7cf2cb5cd
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-26 14:24:40 +0300

    added spaces to the the display

--- lib/engine.rb
+++ lib/engine.rb
@@ -22,10 +22,10 @@ class TicTacToe
   def display
     @pos = positions_with_values
     puts @grid = "
-      #{@pos[1]}|#{@pos[2]}|#{@pos[3]}
+       #{@pos[1]}| #{@pos[2]}| #{@pos[3]}
       -----
-      #{@pos[4]}|#{@pos[5]}|#{@pos[6]}
+       #{@pos[4]}| #{@pos[5]}| #{@pos[6]}
       -----
-      #{@pos[7]}|#{@pos[8]}|#{@pos[9]}"
+       #{@pos[7]}| #{@pos[8]}| #{@pos[9]}"
   end
 end
