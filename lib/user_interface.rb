866bba2db2c1a8ae92f4e406540505c8f90d3ab2
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-26 18:31:26 +0300

    added player 1 and 2 methods

---
+++ lib/user_interface.rb
@@ -0,0 +1,19 @@
+# User interface
+class UserInterface
+  attr_accessor :ply1
+  attr_accessor :ply2
+  def intialize(ply1, ply2)
+    @ply1 = ply1
+    @ply2 = ply2
+  end
+
+  def p1_move(board, position, _engine) # player1 move
+    board.positions_with_values[position] = ply1
+    board.display
+  end
+
+  def p2_move(board, position, _engine) # player2 move
+    board.positions_with_values[position] = ply2
+    board.display
+  end
+end
