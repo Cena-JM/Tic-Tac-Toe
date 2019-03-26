c14313d2d99eb3e692334ac087620876ff7891cd
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-26 18:30:53 +0700

    removed info attr

--- lib/engine.rb
+++ lib/engine.rb
@@ -1,6 +1,6 @@
 # Tic Tac Toe game
 class TicTacToe
-  attr_accessor :info, :positions_with_values
+  attr_accessor :positions_with_values
   WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
                       [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
   def initialize
@@ -29,3 +29,7 @@ class TicTacToe
        #{@pos[7]}| #{@pos[8]}| #{@pos[9]}"
   end
 end
+
+p = TicTacToe.new
+
+p.display
