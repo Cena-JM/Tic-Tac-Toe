8969524a82ce41eeccc52a599af584a846ee23fb
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-26 17:16:16 +0700

    basic display functionality

--- lib/engine.rb
+++ lib/engine.rb
@@ -7,4 +7,24 @@ class TicTacToe
                                '4' => ' ', '5' => ' ', '6' => ' ',
                                '7' => ' ', '8' => ' ', '9' => ' ' }
   end
+
+  def display_positions
+    puts ''
+    puts ' 1 | 2 | 3 '
+    puts '-----------'
+    puts ' 4 | 5 | 6 '
+    puts '-----------'
+    puts ' 7 | 8 | 9 '
+    puts ''
+  end
+
+  def display
+    puts ''
+    puts " #{positions_with_values['1']} | #{positions_with_values['2']} | #{positions_with_values['3']} "
+    puts '-----------'
+    puts " #{positions_with_values['4']} | #{positions_with_values['5']} | #{positions_with_values['6']} "
+    puts '-----------'
+    puts " #{positions_with_values['7']} | #{positions_with_values['8']} | #{positions_with_values['9']} "
+    puts ''
+  end
 end
