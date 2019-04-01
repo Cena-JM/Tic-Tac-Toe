013336789955ffaaa131251569b1c394c3b723ad
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-04-01 03:42:43 +0300

    Divided up Engine class into a three classes plus a run file

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,12 @@
-# class Player
+# Player has players information
+# Player has name, symbol, winner and moves methods
 class Player
-  attr_accessor :name, :symbol
-  def initialize(name)
+  attr_accessor :name, :symbol, :winner, :moves
+
+  def initialize(name, symbol, winner)
     @name = name
     @symbol = symbol
+    @winner = winner
+    @moves = []
   end
 end
